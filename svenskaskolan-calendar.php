686cd44e03d0a9ea7626c7f656abb97044fa8c2f
Andreas McDermott <amcd@amazon.com>
2018-12-31 23:51:58 -0800

    Php

--- svenskaskolan-calendar.php
+++ svenskaskolan-calendar.php
@@ -7,17 +7,68 @@ Author:       Andreas McDermott
 */
   defined( 'ABSPATH' ) or exit;
 
-  function ssc_render_block($attributes, $content) {
+  function format_date($date) {
+    $year = $date['year'];
+    $month = str_pad(strval($date['mon']), 2, "0", STR_PAD_LEFT);
+    $day = str_pad(strval($date['mday']), 2, "0", STR_PAD_LEFT);
+    return "{$year}-{$month}-{$day}";
+  }
+
+  function today_is_sunday() {
+    $date = getdate();
+    $weekday = $date['wday'];
+    return $weekday == 0;
+  }
+
+  function ssc_render_subtitle($attributes, $content) {
     $firstSunday = $attributes['firstSunday'];
     $lastSunday = $attributes['lastSunday'];
-    $schedule = $attributes['schedule'];
 
     $fallYear = explode("-", $firstSunday)[0];
     $springYear = explode("-", $lastSunday)[0];
 
-    $subtitle = "<h3>L&auml;s&aring;ret {$fallYear}-{$springYear}</h3>";
-    $thisSunday = "<div class='ssc-calendar-this-sunday-container'>TODO</div>";
-    $calendar = "<div class='ssc-calendar-container'>TODO</div>";
+    return "<h3>L&auml;s&aring;ret {$fallYear}-{$springYear}</h3>";
+  }
+
+  function ssc_render_this_sunday($attributes, $content) {
+    $nextSunday = today_is_weekday() 
+      ? format_date(getdate())
+      : format_date(getdate(strtotime('next sunday')));
+    $schedule = $attributes['schedule'];
+
+    $item = null;
+    foreach($schedule as $struct) {
+      if ($nextSunday == $struct['date']) {
+          $item = $struct;
+          break;
+      }
+    }
+
+    if ($item == null) return "";
+
+    $date = $item['date'];
+    $isSchoolDay = $item['isSchoolDay'];
+    $time = $item['time'];
+    $notes = $item['notes'];
+    if (!$isSchoolDay) {
+      $notes = "Ingen skola" . (isset($notes) ? " - " . $notes : "");
+    }
+
+    return "<div class='ssc-this-sunday-container'>" .
+      "<strong class='ssc-this-sunday-title'>Nu p&aring; s&ouml;ndag ({$date}):</strong>" .
+      (isset($time) ? "<p class='ssc-this-sunday-time'>{$time}</p>" : "") .
+      (isset($notes) ? "<p class='ssc-this-sunday-notes'>{$notes}</p>" : "") .
+    "</div>";
+  }
+
+  function ssc_render_calendar($attributes, $content) {
+    return "<div class='ssc-calendar-container'>TODO</div>";
+  }
+
+  function ssc_render_block($attributes, $content) {
+    $subtitle = ssc_render_subtitle($attributes, $content);
+    $thisSunday = ssc_render_this_sunday($attributes, $content);
+    $calendar = ssc_render_calendar($attributes, $content);
 
     return "<div>{$subtitle}{$thisSunday}{$calendar}</div>";
   }
